f10c36dab7e05b9f2ba7723d854a55e0f64eb65c
peroutkovah <hanka.peroutkova@centrum.cz>
2024-11-06 09:23:20 +0100

    POkus o rozbehnuti po 6. lekci

--- ToDoList/src/ToDoList.Persistence/Repositories/ToDoItemsRepository.cs
+++ ToDoList/src/ToDoList.Persistence/Repositories/ToDoItemsRepository.cs
@@ -12,10 +12,7 @@ using ToDoList.Domain.Models;
 public class ToDoItemsRepository: IRepository<ToDoItem>
 {
     //zkopirovano z controlleru
-=======
-public class ToDoItemsRepository : IRepository<ToDoItem>
-{
->>>>>>> ec372d91c93f60c082d6094137d2462abbd89a76
+
     private readonly ToDoItemsContext context;
 
     public ToDoItemsRepository(ToDoItemsContext context)
@@ -23,20 +20,14 @@ public class ToDoItemsRepository : IRepository<ToDoItem>
         this.context = context;
     }
 
-<<<<<<< HEAD
-
     //todoitem repository implementuje
-=======
->>>>>>> ec372d91c93f60c082d6094137d2462abbd89a76
+
     public void Create(ToDoItem item)
     {
         context.ToDoItems.Add(item);
         context.SaveChanges();
-<<<<<<< HEAD
+
      }
 }
 
-=======
-    }
-}
->>>>>>> ec372d91c93f60c082d6094137d2462abbd89a76
+
