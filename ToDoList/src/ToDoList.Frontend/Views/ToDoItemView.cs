d6cc114ac9fb7dae78922c5950ac6288a51ae6b1
peroutkovah <hanka.peroutkova@centrum.cz>
2024-11-20 21:52:23 +0100

    Po 9. lekci

---
+++ ToDoList/src/ToDoList.Frontend/Views/ToDoItemView.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ToDoList.Frontend.Views;
+public class ToDoItemView
+{
+    public int ToDoItemId { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public bool IsCompleted { get; set; }
+}
